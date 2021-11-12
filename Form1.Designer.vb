a0a98761cb3dc41758b7fedb01bd0f2bf282ec39
Muffl <thorsten@thts.de>
2021-11-12 10:22:33 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.vb
@@ -0,0 +1,447 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.chkInv = New System.Windows.Forms.CheckBox()
+        Me.cmdBS = New System.Windows.Forms.Button()
+        Me.CmdC = New System.Windows.Forms.Button()
+        Me.cmdCeiling = New System.Windows.Forms.Button()
+        Me.cmdFloor = New System.Windows.Forms.Button()
+        Me.cmdRound = New System.Windows.Forms.Button()
+        Me.cmdTruncate = New System.Windows.Forms.Button()
+        Me.cmdSin = New System.Windows.Forms.Button()
+        Me.cmdCos = New System.Windows.Forms.Button()
+        Me.cmdTan = New System.Windows.Forms.Button()
+        Me.cmdX2 = New System.Windows.Forms.Button()
+        Me.cmdLn = New System.Windows.Forms.Button()
+        Me.cmdLog = New System.Windows.Forms.Button()
+        Me.cmd1durchX = New System.Windows.Forms.Button()
+        Me.cmdPlusMinus = New System.Windows.Forms.Button()
+        Me.cmdPi = New System.Windows.Forms.Button()
+        Me.cmdE = New System.Windows.Forms.Button()
+        Me.cmd7 = New System.Windows.Forms.Button()
+        Me.cmd8 = New System.Windows.Forms.Button()
+        Me.cmd9 = New System.Windows.Forms.Button()
+        Me.cmd4 = New System.Windows.Forms.Button()
+        Me.cmd5 = New System.Windows.Forms.Button()
+        Me.cmd6 = New System.Windows.Forms.Button()
+        Me.cmd1 = New System.Windows.Forms.Button()
+        Me.cmd2 = New System.Windows.Forms.Button()
+        Me.cmd3 = New System.Windows.Forms.Button()
+        Me.cmd0 = New System.Windows.Forms.Button()
+        Me.cmdKomma = New System.Windows.Forms.Button()
+        Me.txtT = New System.Windows.Forms.TextBox()
+        Me.cmdPlus = New System.Windows.Forms.Button()
+        Me.cmdMinus = New System.Windows.Forms.Button()
+        Me.cmdMal = New System.Windows.Forms.Button()
+        Me.cmdGleich = New System.Windows.Forms.Button()
+        Me.cmdGeteilt = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'chkInv
+        '
+        Me.chkInv.AutoSize = True
+        Me.chkInv.Location = New System.Drawing.Point(35, 76)
+        Me.chkInv.Name = "chkInv"
+        Me.chkInv.Size = New System.Drawing.Size(41, 17)
+        Me.chkInv.TabIndex = 0
+        Me.chkInv.Text = "Inv"
+        Me.chkInv.UseVisualStyleBackColor = True
+        '
+        'cmdBS
+        '
+        Me.cmdBS.Location = New System.Drawing.Point(274, 99)
+        Me.cmdBS.Name = "cmdBS"
+        Me.cmdBS.Size = New System.Drawing.Size(36, 23)
+        Me.cmdBS.TabIndex = 1
+        Me.cmdBS.Text = "BS"
+        Me.cmdBS.UseVisualStyleBackColor = True
+        '
+        'CmdC
+        '
+        Me.CmdC.Location = New System.Drawing.Point(316, 99)
+        Me.CmdC.Name = "CmdC"
+        Me.CmdC.Size = New System.Drawing.Size(36, 23)
+        Me.CmdC.TabIndex = 2
+        Me.CmdC.Text = "C"
+        Me.CmdC.UseVisualStyleBackColor = True
+        '
+        'cmdCeiling
+        '
+        Me.cmdCeiling.Location = New System.Drawing.Point(274, 144)
+        Me.cmdCeiling.Name = "cmdCeiling"
+        Me.cmdCeiling.Size = New System.Drawing.Size(78, 23)
+        Me.cmdCeiling.TabIndex = 3
+        Me.cmdCeiling.Text = "Ceiling"
+        Me.cmdCeiling.UseVisualStyleBackColor = True
+        '
+        'cmdFloor
+        '
+        Me.cmdFloor.Location = New System.Drawing.Point(274, 173)
+        Me.cmdFloor.Name = "cmdFloor"
+        Me.cmdFloor.Size = New System.Drawing.Size(78, 23)
+        Me.cmdFloor.TabIndex = 4
+        Me.cmdFloor.Text = "Floor"
+        Me.cmdFloor.UseVisualStyleBackColor = True
+        '
+        'cmdRound
+        '
+        Me.cmdRound.Location = New System.Drawing.Point(274, 202)
+        Me.cmdRound.Name = "cmdRound"
+        Me.cmdRound.Size = New System.Drawing.Size(78, 23)
+        Me.cmdRound.TabIndex = 5
+        Me.cmdRound.Text = "Round"
+        Me.cmdRound.UseVisualStyleBackColor = True
+        '
+        'cmdTruncate
+        '
+        Me.cmdTruncate.Location = New System.Drawing.Point(274, 231)
+        Me.cmdTruncate.Name = "cmdTruncate"
+        Me.cmdTruncate.Size = New System.Drawing.Size(78, 23)
+        Me.cmdTruncate.TabIndex = 6
+        Me.cmdTruncate.Text = "Truncate"
+        Me.cmdTruncate.UseVisualStyleBackColor = True
+        '
+        'cmdSin
+        '
+        Me.cmdSin.Location = New System.Drawing.Point(35, 99)
+        Me.cmdSin.Name = "cmdSin"
+        Me.cmdSin.Size = New System.Drawing.Size(36, 23)
+        Me.cmdSin.TabIndex = 7
+        Me.cmdSin.Text = "sin"
+        Me.cmdSin.UseVisualStyleBackColor = True
+        '
+        'cmdCos
+        '
+        Me.cmdCos.Location = New System.Drawing.Point(35, 128)
+        Me.cmdCos.Name = "cmdCos"
+        Me.cmdCos.Size = New System.Drawing.Size(36, 23)
+        Me.cmdCos.TabIndex = 8
+        Me.cmdCos.Text = "cos"
+        Me.cmdCos.UseVisualStyleBackColor = True
+        '
+        'cmdTan
+        '
+        Me.cmdTan.Location = New System.Drawing.Point(35, 157)
+        Me.cmdTan.Name = "cmdTan"
+        Me.cmdTan.Size = New System.Drawing.Size(36, 23)
+        Me.cmdTan.TabIndex = 9
+        Me.cmdTan.Text = "tan"
+        Me.cmdTan.UseVisualStyleBackColor = True
+        '
+        'cmdX2
+        '
+        Me.cmdX2.Location = New System.Drawing.Point(77, 99)
+        Me.cmdX2.Name = "cmdX2"
+        Me.cmdX2.Size = New System.Drawing.Size(36, 23)
+        Me.cmdX2.TabIndex = 10
+        Me.cmdX2.Text = "x2"
+        Me.cmdX2.UseVisualStyleBackColor = True
+        '
+        'cmdLn
+        '
+        Me.cmdLn.Location = New System.Drawing.Point(77, 128)
+        Me.cmdLn.Name = "cmdLn"
+        Me.cmdLn.Size = New System.Drawing.Size(36, 23)
+        Me.cmdLn.TabIndex = 11
+        Me.cmdLn.Text = "ln"
+        Me.cmdLn.UseVisualStyleBackColor = True
+        '
+        'cmdLog
+        '
+        Me.cmdLog.Location = New System.Drawing.Point(77, 157)
+        Me.cmdLog.Name = "cmdLog"
+        Me.cmdLog.Size = New System.Drawing.Size(36, 23)
+        Me.cmdLog.TabIndex = 12
+        Me.cmdLog.Text = "log"
+        Me.cmdLog.UseVisualStyleBackColor = True
+        '
+        'cmd1durchX
+        '
+        Me.cmd1durchX.Location = New System.Drawing.Point(77, 231)
+        Me.cmd1durchX.Name = "cmd1durchX"
+        Me.cmd1durchX.Size = New System.Drawing.Size(36, 23)
+        Me.cmd1durchX.TabIndex = 13
+        Me.cmd1durchX.Text = "1/x"
+        Me.cmd1durchX.UseVisualStyleBackColor = True
+        '
+        'cmdPlusMinus
+        '
+        Me.cmdPlusMinus.Location = New System.Drawing.Point(35, 231)
+        Me.cmdPlusMinus.Name = "cmdPlusMinus"
+        Me.cmdPlusMinus.Size = New System.Drawing.Size(36, 23)
+        Me.cmdPlusMinus.TabIndex = 14
+        Me.cmdPlusMinus.Text = "+/-"
+        Me.cmdPlusMinus.UseVisualStyleBackColor = True
+        '
+        'cmdPi
+        '
+        Me.cmdPi.Location = New System.Drawing.Point(129, 231)
+        Me.cmdPi.Name = "cmdPi"
+        Me.cmdPi.Size = New System.Drawing.Size(36, 23)
+        Me.cmdPi.TabIndex = 15
+        Me.cmdPi.Text = "PI"
+        Me.cmdPi.UseVisualStyleBackColor = True
+        '
+        'cmdE
+        '
+        Me.cmdE.Location = New System.Drawing.Point(171, 231)
+        Me.cmdE.Name = "cmdE"
+        Me.cmdE.Size = New System.Drawing.Size(36, 23)
+        Me.cmdE.TabIndex = 16
+        Me.cmdE.Text = "E"
+        Me.cmdE.UseVisualStyleBackColor = True
+        '
+        'cmd7
+        '
+        Me.cmd7.Location = New System.Drawing.Point(133, 99)
+        Me.cmd7.Name = "cmd7"
+        Me.cmd7.Size = New System.Drawing.Size(23, 23)
+        Me.cmd7.TabIndex = 17
+        Me.cmd7.Text = "7"
+        Me.cmd7.UseVisualStyleBackColor = True
+        '
+        'cmd8
+        '
+        Me.cmd8.Location = New System.Drawing.Point(162, 99)
+        Me.cmd8.Name = "cmd8"
+        Me.cmd8.Size = New System.Drawing.Size(23, 23)
+        Me.cmd8.TabIndex = 18
+        Me.cmd8.Text = "8"
+        Me.cmd8.UseVisualStyleBackColor = True
+        '
+        'cmd9
+        '
+        Me.cmd9.Location = New System.Drawing.Point(191, 99)
+        Me.cmd9.Name = "cmd9"
+        Me.cmd9.Size = New System.Drawing.Size(23, 23)
+        Me.cmd9.TabIndex = 19
+        Me.cmd9.Text = "9"
+        Me.cmd9.UseVisualStyleBackColor = True
+        '
+        'cmd4
+        '
+        Me.cmd4.Location = New System.Drawing.Point(133, 128)
+        Me.cmd4.Name = "cmd4"
+        Me.cmd4.Size = New System.Drawing.Size(23, 23)
+        Me.cmd4.TabIndex = 20
+        Me.cmd4.Text = "4"
+        Me.cmd4.UseVisualStyleBackColor = True
+        '
+        'cmd5
+        '
+        Me.cmd5.Location = New System.Drawing.Point(162, 128)
+        Me.cmd5.Name = "cmd5"
+        Me.cmd5.Size = New System.Drawing.Size(23, 23)
+        Me.cmd5.TabIndex = 21
+        Me.cmd5.Text = "5"
+        Me.cmd5.UseVisualStyleBackColor = True
+        '
+        'cmd6
+        '
+        Me.cmd6.Location = New System.Drawing.Point(191, 128)
+        Me.cmd6.Name = "cmd6"
+        Me.cmd6.Size = New System.Drawing.Size(23, 23)
+        Me.cmd6.TabIndex = 22
+        Me.cmd6.Text = "6"
+        Me.cmd6.UseVisualStyleBackColor = True
+        '
+        'cmd1
+        '
+        Me.cmd1.Location = New System.Drawing.Point(133, 157)
+        Me.cmd1.Name = "cmd1"
+        Me.cmd1.Size = New System.Drawing.Size(23, 23)
+        Me.cmd1.TabIndex = 23
+        Me.cmd1.Text = "1"
+        Me.cmd1.UseVisualStyleBackColor = True
+        '
+        'cmd2
+        '
+        Me.cmd2.Location = New System.Drawing.Point(162, 157)
+        Me.cmd2.Name = "cmd2"
+        Me.cmd2.Size = New System.Drawing.Size(23, 23)
+        Me.cmd2.TabIndex = 24
+        Me.cmd2.Text = "2"
+        Me.cmd2.UseVisualStyleBackColor = True
+        '
+        'cmd3
+        '
+        Me.cmd3.Location = New System.Drawing.Point(191, 157)
+        Me.cmd3.Name = "cmd3"
+        Me.cmd3.Size = New System.Drawing.Size(23, 23)
+        Me.cmd3.TabIndex = 25
+        Me.cmd3.Text = "3"
+        Me.cmd3.UseVisualStyleBackColor = True
+        '
+        'cmd0
+        '
+        Me.cmd0.Location = New System.Drawing.Point(162, 186)
+        Me.cmd0.Name = "cmd0"
+        Me.cmd0.Size = New System.Drawing.Size(23, 23)
+        Me.cmd0.TabIndex = 26
+        Me.cmd0.Text = "0"
+        Me.cmd0.UseVisualStyleBackColor = True
+        '
+        'cmdKomma
+        '
+        Me.cmdKomma.Location = New System.Drawing.Point(191, 186)
+        Me.cmdKomma.Name = "cmdKomma"
+        Me.cmdKomma.Size = New System.Drawing.Size(23, 23)
+        Me.cmdKomma.TabIndex = 27
+        Me.cmdKomma.Text = ","
+        Me.cmdKomma.UseVisualStyleBackColor = True
+        '
+        'txtT
+        '
+        Me.txtT.Location = New System.Drawing.Point(35, 31)
+        Me.txtT.Name = "txtT"
+        Me.txtT.Size = New System.Drawing.Size(291, 20)
+        Me.txtT.TabIndex = 28
+        Me.txtT.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'cmdPlus
+        '
+        Me.cmdPlus.Location = New System.Drawing.Point(225, 99)
+        Me.cmdPlus.Name = "cmdPlus"
+        Me.cmdPlus.Size = New System.Drawing.Size(27, 23)
+        Me.cmdPlus.TabIndex = 29
+        Me.cmdPlus.Text = "+"
+        Me.cmdPlus.UseVisualStyleBackColor = False
+        '
+        'cmdMinus
+        '
+        Me.cmdMinus.Location = New System.Drawing.Point(225, 128)
+        Me.cmdMinus.Name = "cmdMinus"
+        Me.cmdMinus.Size = New System.Drawing.Size(27, 23)
+        Me.cmdMinus.TabIndex = 30
+        Me.cmdMinus.Text = "-"
+        Me.cmdMinus.UseVisualStyleBackColor = True
+        '
+        'cmdMal
+        '
+        Me.cmdMal.Location = New System.Drawing.Point(225, 157)
+        Me.cmdMal.Name = "cmdMal"
+        Me.cmdMal.Size = New System.Drawing.Size(27, 23)
+        Me.cmdMal.TabIndex = 31
+        Me.cmdMal.Text = "*"
+        Me.cmdMal.UseVisualStyleBackColor = True
+        '
+        'cmdGleich
+        '
+        Me.cmdGleich.Location = New System.Drawing.Point(213, 231)
+        Me.cmdGleich.Name = "cmdGleich"
+        Me.cmdGleich.Size = New System.Drawing.Size(39, 23)
+        Me.cmdGleich.TabIndex = 32
+        Me.cmdGleich.Text = "="
+        Me.cmdGleich.UseVisualStyleBackColor = True
+        '
+        'cmdGeteilt
+        '
+        Me.cmdGeteilt.Location = New System.Drawing.Point(225, 186)
+        Me.cmdGeteilt.Name = "cmdGeteilt"
+        Me.cmdGeteilt.Size = New System.Drawing.Size(27, 23)
+        Me.cmdGeteilt.TabIndex = 33
+        Me.cmdGeteilt.Text = "/"
+        Me.cmdGeteilt.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(364, 288)
+        Me.Controls.Add(Me.cmdGeteilt)
+        Me.Controls.Add(Me.cmdGleich)
+        Me.Controls.Add(Me.cmdMal)
+        Me.Controls.Add(Me.cmdMinus)
+        Me.Controls.Add(Me.cmdPlus)
+        Me.Controls.Add(Me.txtT)
+        Me.Controls.Add(Me.cmdKomma)
+        Me.Controls.Add(Me.cmd0)
+        Me.Controls.Add(Me.cmd3)
+        Me.Controls.Add(Me.cmd2)
+        Me.Controls.Add(Me.cmd1)
+        Me.Controls.Add(Me.cmd6)
+        Me.Controls.Add(Me.cmd5)
+        Me.Controls.Add(Me.cmd4)
+        Me.Controls.Add(Me.cmd9)
+        Me.Controls.Add(Me.cmd8)
+        Me.Controls.Add(Me.cmd7)
+        Me.Controls.Add(Me.cmdE)
+        Me.Controls.Add(Me.cmdPi)
+        Me.Controls.Add(Me.cmdPlusMinus)
+        Me.Controls.Add(Me.cmd1durchX)
+        Me.Controls.Add(Me.cmdLog)
+        Me.Controls.Add(Me.cmdLn)
+        Me.Controls.Add(Me.cmdX2)
+        Me.Controls.Add(Me.cmdTan)
+        Me.Controls.Add(Me.cmdCos)
+        Me.Controls.Add(Me.cmdSin)
+        Me.Controls.Add(Me.cmdTruncate)
+        Me.Controls.Add(Me.cmdRound)
+        Me.Controls.Add(Me.cmdFloor)
+        Me.Controls.Add(Me.cmdCeiling)
+        Me.Controls.Add(Me.CmdC)
+        Me.Controls.Add(Me.cmdBS)
+        Me.Controls.Add(Me.chkInv)
+        Me.Name = "Form1"
+        Me.Text = "Mathematik"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents chkInv As CheckBox
+    Friend WithEvents cmdBS As Button
+    Friend WithEvents CmdC As Button
+    Friend WithEvents cmdCeiling As Button
+    Friend WithEvents cmdFloor As Button
+    Friend WithEvents cmdRound As Button
+    Friend WithEvents cmdTruncate As Button
+    Friend WithEvents cmdSin As Button
+    Friend WithEvents cmdCos As Button
+    Friend WithEvents cmdTan As Button
+    Friend WithEvents cmdX2 As Button
+    Friend WithEvents cmdLn As Button
+    Friend WithEvents cmdLog As Button
+    Friend WithEvents cmd1durchX As Button
+    Friend WithEvents cmdPlusMinus As Button
+    Friend WithEvents cmdPi As Button
+    Friend WithEvents cmdE As Button
+    Friend WithEvents cmd7 As Button
+    Friend WithEvents cmd8 As Button
+    Friend WithEvents cmd9 As Button
+    Friend WithEvents cmd4 As Button
+    Friend WithEvents cmd5 As Button
+    Friend WithEvents cmd6 As Button
+    Friend WithEvents cmd1 As Button
+    Friend WithEvents cmd2 As Button
+    Friend WithEvents cmd3 As Button
+    Friend WithEvents cmd0 As Button
+    Friend WithEvents cmdKomma As Button
+    Friend WithEvents txtT As TextBox
+    Friend WithEvents cmdPlus As Button
+    Friend WithEvents cmdMinus As Button
+    Friend WithEvents cmdMal As Button
+    Friend WithEvents cmdGleich As Button
+    Friend WithEvents cmdGeteilt As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,128 @@
+﻿
+
+
+Public Class Form1
+    Dim x As Double
+    Dim y As Double
+
+
+
+    Private Sub txtT_TextChanged(sender As Object, e As EventArgs) Handles txtT.TextChanged
+        Try
+            x = Convert.ToDouble(txtT.Text)
+
+        Catch ex As Exception
+            txtT.Text = ""
+            x = 0
+            y = 0
+
+
+        End Try
+    End Sub
+
+    Private Sub cmdCeiling_Click(sender As Object, e As EventArgs) Handles cmdCeiling.Click
+        txtT.Text = Math.Ceiling(x)
+    End Sub
+
+    Private Sub cmdFloor_Click(sender As Object, e As EventArgs) Handles cmdFloor.Click
+        txtT.Text = Math.Floor(x)
+    End Sub
+
+    Private Sub cmdRound_Click(sender As Object, e As EventArgs) Handles cmdRound.Click
+        txtT.Text = Math.Round(x)
+    End Sub
+
+    Private Sub cmdTruncate_Click(sender As Object, e As EventArgs) Handles cmdTruncate.Click
+        txtT.Text = Math.Truncate(x)
+    End Sub
+
+    Private Sub CmdC_Click(sender As Object, e As EventArgs) Handles CmdC.Click
+        txtT.Text = ""
+    End Sub
+
+    Private Sub cmdBS_Click(sender As Object, e As EventArgs) Handles cmdBS.Click
+        txtT.Text = txtT.Text.Substring(0, txtT.Text.Length - 1)
+    End Sub
+
+    Private Sub cmdPlusMinus_Click(sender As Object, e As EventArgs) Handles cmdPlusMinus.Click
+        txtT.Text = x * -1.0
+    End Sub
+
+    Private Sub cmd1durchX_Click(sender As Object, e As EventArgs) Handles cmd1durchX.Click
+        txtT.Text = 1.0 / x
+    End Sub
+
+    Private Sub cmdPi_Click(sender As Object, e As EventArgs) Handles cmdPi.Click
+        txtT.Text = Math.PI
+    End Sub
+
+    Private Sub cmdE_Click(sender As Object, e As EventArgs) Handles cmdE.Click
+        txtT.Text = Math.E
+    End Sub
+
+    Private Sub cmd0_Click(sender As Object, e As EventArgs) Handles cmd0.Click, cmd9.Click, cmd8.Click, cmd7.Click, cmd6.Click, cmd5.Click, cmd4.Click, cmd3.Click, cmd2.Click, cmd1.Click
+        txtT.Text &= sender.Text
+    End Sub
+
+    Private Sub cmdKomma_Click(sender As Object, e As EventArgs) Handles cmdKomma.Click
+        If txtT.Text.IndexOf(",") < 0 Then
+            txtT.Text &= ","
+        End If
+    End Sub
+
+    Private Sub cmdX2_Click(sender As Object, e As EventArgs) Handles cmdX2.Click
+        If chkInv.Checked Then
+            txtT.Text = Math.Sqrt(x)
+        Else
+            txtT.Text = Math.Pow(x, 2.0)
+        End If
+    End Sub
+
+    Private Sub cmdSin_Click(sender As Object, e As EventArgs) Handles cmdSin.Click
+        If chkInv.Checked Then
+            txtT.Text = Math.Asin(x) * 180 / Math.PI
+            chkInv.Checked = False
+        Else
+            txtT.Text = Math.Sin(x / 180.0 * Math.PI)
+        End If
+
+    End Sub
+
+    Private Sub cmdCos_Click(sender As Object, e As EventArgs) Handles cmdCos.Click
+        If chkInv.Checked Then
+            txtT.Text = Math.Acos(x) * 180 / Math.PI
+            chkInv.Checked = False
+        Else
+            txtT.Text = Math.Cos(x / 180.0 * Math.PI)
+        End If
+    End Sub
+
+    Private Sub cmdTan_Click(sender As Object, e As EventArgs) Handles cmdTan.Click
+        If chkInv.Checked Then
+            txtT.Text = Math.Atan(x) * 180 / Math.PI
+            chkInv.Checked = False
+        Else
+            txtT.Text = Math.Tan(x / 180.0 * Math.PI)
+        End If
+    End Sub
+
+    Private Sub cmdLn_Click(sender As Object, e As EventArgs) Handles cmdLn.Click
+        If chkInv.Checked Then
+            txtT.Text = Math.Exp(x)
+            chkInv.Checked = False
+        Else
+            txtT.Text = Math.Log(x)
+        End If
+    End Sub
+
+    Private Sub cmdLog_Click(sender As Object, e As EventArgs) Handles cmdLog.Click
+        If chkInv.Checked Then
+            txtT.Text = Math.Pow(10.0, x)
+            chkInv.Checked = False
+        Else
+            txtT.Text = Math.Log10(x)
+        End If
+    End Sub
+
+
+End Class
